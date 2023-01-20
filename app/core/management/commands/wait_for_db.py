7c08347294de2401985f6cfe341dfada03a81d42
Ivan Beira <ivanbeira@gmail.com>
2023-01-20 17:36:35 -0300

    Configured checks and docker compose to wait for database

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -21,6 +21,12 @@ class Command(BaseCommand):
             try:
                 self.check(databases=['default'])
                 db_up = True
+            #TODO: Fix this, it's not working as expected. The first except block \
+            # should left out the Psycopg2OpError and OperationalError.
+            except Exception as e:
+                if isinstance(e, (Psycopg2OpError, OperationalError)):
+                    self.stdout.write(f'An error occurred: {e}')
+                    raise e
             except (Psycopg2OpError, OperationalError):
                 self.stdout.write('Database unavailable, waiting 1 second...')
                 time.sleep(1)
